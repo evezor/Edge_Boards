236fe00f043c5b830e2da592249c570dc5461360
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2020-08-27 14:49:39 -0500

    Bring master up to date with Carl's fork

--- poc/board2.py
+++ poc/board2.py
@@ -5,40 +5,69 @@ import time
 
 from ocan import OCan
 
-import bits
+def boot(ocan):
 
+    print("booting...")
+    print("state 0")
 
-def set_filter(can):
-    can.setfilter(0, CAN.MASK32, 0, (0,0) )
-    # can.setfilter(1, CAN.MASK32, 0, (0b11111111111111111111111111111,0) )
-    # can.setfilter(2, CAN.MASK32, 0, (0,0b11111111111111111111111111111) )
-    # can.setfilter(3, CAN.MASK32, 0, ( 0b11111111111111111111111111111,
-    #    0b11111111111111111111111111111) )
+    # >>> binascii.unhexlify('ff0000ff')
+    # b'\xff\x00\x00\xff'
+    # mac = b'\xff\x00\x00\xff'
+    mac = machine.unique_id()[-5:]
+
+    print("state 1")
+    # hello zorg, I am here
+    ocan.send("NWK", 0, 0, b'')
+
+    # wait for Zorg to wake up
+    ocan._setfilter(0, (0,0) )
+    beer = ocan.recieve()
+    # zorg: who are you?
+    print("state 2")
+
+    # I am foof:
+    ocan.send("NWK", 0, 1, mac )
+
+    # wait for Zorg to assign a can_id
+    ocan._setfilter(0, (0,0) )
+    can_id = None
+    while can_id is None:
+        beer = ocan.recieve()
+        print("state 3")
+        hacky_1 = 1
+        if beer.bonus == hacky_1 and beer.data == mac:
+            can_id = beer.cid
+            break
+
+    print("booted!")
+
 
 
 def drink(ocan):
+
+    ocan._setfilter(0, (0,0) )
+
     print("waiting...")
 
-    last_id = None
+    last_num = None
     while True:
 
-        can_id, rtr, fmi, data = ocan._recieve()
+        beer = ocan.recieve()
+        print(beer.channel, beer.cid, beer.bonus, beer.data, end=' ')
 
-        print(can_id, end=' ')
-
-        if last_id is not None:
-            assert can_id == last_id + 1
+        if last_num is not None:
+            assert beer.bonus == last_num + 1, "sequance broken by {}".format(beer.bonus - last_num)
             ticks = time.ticks_ms() - last_tick
             print("*"*ticks)
 
-        last_id = can_id
+        last_num = beer.bonus
         last_tick = time.ticks_ms()
 
 
 def main():
     ocan = OCan()
-    ocan._setfilter(0, (0,0) )
-    drink(ocan)
+    # drink(ocan)
+    boot(ocan)
 
 
 if __name__=='__main__':

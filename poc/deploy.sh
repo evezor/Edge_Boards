9aadba08b8c1f237b8f8441144c4a8eef7398989
Carl Karsten <carl@personnelware.com>
2020-09-02 02:27:02 -0500

    wipe and setup 3 boards plugged into Carl's laptop.

---
+++ poc/deploy.sh
@@ -0,0 +1,12 @@
+set -ex
+
+rm /media/carl/PYBFLASH/*
+
+cp main.py bits.py ocan.py board?.py /media/carl/PYBFLASH
+cp main.py bits.py ocan.py board?.py /media/carl/PYBFLASH1
+cp main.py bits.py ocan.py board?.py /media/carl/PYBFLASH2
+
+cp boards/zorg/* /media/carl/PYBFLASH
+cp boards/b2/* /media/carl/PYBFLASH1
+cp boards/b3/* /media/carl/PYBFLASH2
+

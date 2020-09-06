9a62c3cd3f959f3da6a250ae1c7b1439f3d28566
Carl Karsten <carl@personnelware.com>
2020-09-06 02:23:51 -0500

    deploy 4 boards.

--- poc/deploy.sh
+++ poc/deploy.sh
@@ -1,12 +1,14 @@
 set -ex
 
-rm /media/carl/PYBFLASH/*
+# rm /media/carl/PYBFLASH*/*
 
 cp main.py bits.py ocan.py board?.py /media/carl/PYBFLASH
 cp main.py bits.py ocan.py board?.py /media/carl/PYBFLASH1
 cp main.py bits.py ocan.py board?.py /media/carl/PYBFLASH2
 
 cp boards/zorg/* /media/carl/PYBFLASH
+
 cp boards/b2/* /media/carl/PYBFLASH1
 cp boards/b3/* /media/carl/PYBFLASH2
+cp boards/b3/* /media/carl/PYBFLASH3
 

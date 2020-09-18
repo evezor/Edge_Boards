4a9fa072fd35c3186b0e91b00165677caefb8a9c
Carl Karsten <carl@personnelware.com>
2020-09-18 00:34:35 -0500

    change lights to leds.

--- poc/boards/b3/b3.py
+++ poc/boards/b3/b3.py
@@ -20,7 +20,7 @@ BUTTON={
 class B3(Driver):
 
     can_chip_pins = []
-    light_pins = []
+    led_pins = []
     button_pins = []
 
     def setup_pins(self):
@@ -41,7 +41,7 @@ class B3(Driver):
             ]
 
         # outputs:
-        self.light_pins = [
+        self.led_pins = [
             Pin("D5", Pin.OUT),
             Pin("D13", Pin.OUT),
             ]
@@ -54,7 +54,7 @@ class B3(Driver):
         # ValueError: Pin(C1) doesn't have an af for Timer(3)
 
         tim = Timer(3, freq=1000)
-        self.ch = tim.channel(2, Timer.PWM, pin=self.light_pins[0])
+        self.ch = tim.channel(2, Timer.PWM, pin=self.led_pins[0])
         self.ch.pulse_width_percent(5)
 
 
@@ -70,9 +70,9 @@ class B3(Driver):
     def halt(self):
         print("halt.")
         while True:
-            self.light_0_on()
+            self.led_0_on()
             time.sleep(.1)
-            self.light_0_off()
+            self.led_0_off()
             time.sleep(.1)
 
     # dangers:
@@ -151,23 +151,23 @@ class B3(Driver):
 
     # outputs:
 
-    def light_oo(self, light_no, oo):
-        # turn a light on or off
+    def led_oo(self, led_no, oo):
+        # turn a led on or off
 
         v = 0 if oo == "off" else 1
-        self.light_pins[light_no].value(v)
+        self.led_pins[led_no].value(v)
 
-    def light_0_on(self):
-        self.light_oo(0, "on")
+    def led_0_on(self):
+        self.led_oo(0, "on")
 
-    def light_0_off(self):
-        self.light_oo(0, "off")
+    def led_0_off(self):
+        self.led_oo(0, "off")
 
-    def light_1_on(self):
-        self.light_oo(1, "on")
+    def led_1_on(self):
+        self.led_oo(1, "on")
 
-    def light_1_off(self):
-        self.light_oo(1, "off")
+    def led_1_off(self):
+        self.led_oo(1, "off")
 
     def led_0_dim(self):
 

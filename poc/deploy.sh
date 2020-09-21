1d0ad9b41ee8ca97e2f56ed55fa6e475c4230039
Carl Karsten <carl@personnelware.com>
2020-09-21 05:14:38 -0500

    only deploy files to boards that need updateing

--- poc/deploy.sh
+++ poc/deploy.sh
@@ -1,17 +1,19 @@
 set -ex
 
-# rm /media/carl/PYBFLASH*/*
+board_zorg=/media/carl/PYBFLASH
+board_a=/media/carl/PYBFLASH1
+board_b=/media/carl/PYBFLASH3
+board_c=/media/carl/PYBFLASH2
 
-cp main.py bundle.py ocan.py board.py /media/carl/PYBFLASH
-cp main.py bundle.py ocan.py board.py edge.py driver.py /media/carl/PYBFLASH1
-cp main.py bundle.py ocan.py board.py edge.py driver.py /media/carl/PYBFLASH2
-cp main.py bundle.py ocan.py board.py edge.py driver.py /media/carl/PYBFLASH3
+cp -u main.py bundle.py ocan.py board.py $board_zorg
+for board in $board_a $board_b $board_c; do
+    cp -u main.py bundle.py ocan.py board.py edge.py driver.py $board
+done
 
-cp boards/zorg/* /media/carl/PYBFLASH
-cp systems/mapo.json systems/ls2oh/commission.json /media/carl/PYBFLASH
+cp -u boards/zorg/* $board_zorg
+cp -u systems/mapo.json systems/ls2oh/commission.json $board_zorg
 
-cp boards/b2/* /media/carl/PYBFLASH1
+cp -u boards/b3/* $board_a
+cp -u boards/b3/* $board_b
 
-cp boards/b3/* /media/carl/PYBFLASH2
-cp boards/b3/* /media/carl/PYBFLASH3
-
+cp -u boards/b2/* $board_c

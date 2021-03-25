d434f55ed30fb6757a78da88c4987f2108c948a3
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2021-03-25 16:01:29 -0500

    Looking for a better solution to setup hardware

--- boards/edges/b3/b3.py
+++ boards/edges/b3/b3.py
@@ -1,5 +1,18 @@
 from driver import Driver
 
+    def setup_hardware():
+        led0 = Pin("D5", Pin.OUT)
+        led1 = Pin("D13", Pin.OUT)
+
+        led0_tim = Timer(3, freq=1000)
+        led0_tim.channel(2, Timer.PWM, pin=led0 ))
+
+        button0 = Pin("D12", Pin.IN, Pin.PULL_UP)
+        button1 = Pin("D11", Pin.IN, Pin.PULL_UP)
+
+        pot0 = pyb.ADC("A0")
+
+
 class B3(Driver):
 
     def button_0_on(self):
@@ -28,4 +41,3 @@ class B3(Driver):
 
     def led_0_dim(self, value):
         return self.led_dim( 'led_0_dim', value )
-

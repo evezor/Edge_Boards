480d8e0e8334c813cf02be6d2bb886810b746c73
Carl Karsten <carl@personnelware.com>
2021-04-18 19:02:41 -0500

    setup an Edge board.

---
+++ tools/mk_edge.sh
@@ -0,0 +1,29 @@
+set -ex
+
+# args: board name, mount point
+
+# Edge board setup
+# Step 1: MicroPython
+# Step 2: boot.py, edge.py, driver.py $board_name.py (and some others)
+
+board=$1
+dst=${2:-/media/$USER/PYBFLASH}
+
+rm -f $dst/state.json
+
+(
+cd boards
+
+(
+cd emu
+cp -u main.py bundle.py ocan.py board.py edge.py $dst
+)
+
+(
+cd edges
+cp -u driver.py $dst
+cp -u $board/* $dst
+
+)
+)
+
